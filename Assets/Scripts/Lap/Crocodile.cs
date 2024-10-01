8c4dcb8e64cb1af6eed7966f9d86bd1e4e6d85bf
BaBaBuaB <arm5052sueksa@gmail.com>
2024-10-01 11:10:22 +0700

    Make code follow class
    -do Aligrator Behavior

--- Assets/Scripts/Lap/Crocodile.cs
+++ Assets/Scripts/Lap/Crocodile.cs
@@ -4,19 +4,50 @@ using UnityEngine;
 
 public class Crocodile : Enemy
 {
-    private float attackRange;
-    private Player player;
+    [SerializeField]private float attackRange;
+    [SerializeField]private Player player;
+
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform bulletSpawnPoint;
+    [SerializeField] private float bulletWaitTime;
+    [SerializeField
+        ] private float bulletTimer;
 
     private void Start()
     {
         Init(100);
         Debug.Log($"Ant's Hp = {Health}");
+    }
+
+    private void Update()
+    {
+        bulletTimer -= Time.deltaTime;
 
         Behaviour();
+
+        if (bulletTimer < 0)
+        {
+            bulletTimer = bulletWaitTime;
+        }
     }
 
     public override void Behaviour()
     {
-        Debug.Log("CrocoDile.Behaviour");
+        Vector2 direction = player.transform.position-transform.position;
+        float distance = direction.magnitude;
+
+        if (distance < attackRange)
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        if (bulletTimer == bulletWaitTime)
+        {
+            bullet = Instantiate(bullet, bulletSpawnPoint.position, Quaternion.identity);
+
+        }
     }
 }
